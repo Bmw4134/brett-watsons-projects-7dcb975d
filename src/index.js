ebe7bd52ece88f2b8ce463d49add7bee75dc4ad9
Bmw4134 <bm.watson34@gmail.com>
2025-06-11 23:41:38 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import KaizenPortfolio from './pages/KaizenPortfolio';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <KaizenPortfolio />
+  </React.StrictMode>
+);
