bdaa73efbbcb58135a005dad3a6c3fb55c0c70c8
Bmw4134 <bm.watson34@gmail.com>
2025-06-12 02:07:58 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dashboard from "./Dashboard";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Dashboard />);
